3ca089f2993e9dc60791f8f5821d6bb75589adc6
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-10-13 19:19:29 -0300

    Update Atv6 - Para casa.c

--- Semana 2/Atv6 - Para casa.c
+++ Semana 2/Atv6 - Para casa.c
@@ -1,6 +1,7 @@
 /*
     Escreva um programa em C que peça ao usuário o salário e o tempo de trabalho em anos.
     O programa deverá calcular um bônus de 5% sobre o salário se o tempo de trabalho for superior a 5 anos.
+    
     Entrada = Pedir ao usuario o salario que ele recebe atualmente e o trabalho em anos.
     processamento = calcular um bônus de 5% caso a pessoa trabalhe mais que 5 anos.
     saida = mostrar o resultado
